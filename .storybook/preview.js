fb898dc4a8d431b25b648031815b4e267e4098a5
Daniel Farrell <highdanbo@gmail.com>
2021-03-07 01:54:51 +0000

    feat: add ThemeVariables component for easy setup of CSS variables

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,26 @@
+import React from "react";
+import { ThemeVariables } from "../src/components/ThemeVariables/ThemeVariables";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: {
-      order: ["Introduction", "Foundations", ["Typography", "Colour"], "Atoms"],
+      order: [
+        "Introduction",
+        "Foundations",
+        ["Typography", "Colour"],
+        "Guides",
+        "Atoms",
+      ],
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <div>
+      <ThemeVariables />
+      <Story />
+    </div>
+  ),
+];
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ export { Alert } from "./components/Alert/Alert";
 export { Button } from "./components/Button/Button";
 export { ScreenReader } from "./components/ScreenReader/ScreenReader";
 
+// Misc
+export { ThemeVariables } from "./components/ThemeVariables/ThemeVariables";
+
 // Icons
 export { ErrorIcon } from "./components/Icons/ErrorIcon";
 export { InfoIcon } from "./components/Icons/InfoIcon";
